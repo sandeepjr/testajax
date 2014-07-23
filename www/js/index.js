4e8221a46d639a2bb584058d04b57a232007bfd1
sandeepjr <sandeep.ramachandran@bs-shipmanagement.com>
2014-07-23 17:13:07 +0530

    added error

--- www/js/index.js
+++ www/js/index.js
@@ -89,3 +89,8 @@ function goText() {
         }
     });
 }
+
+window.onerror = function(msg, url, linenumber) {
+    alert('Error message: '+msg+'\nURL: '+url+'\nLine Number: '+linenumber);
+    return true;
+}
